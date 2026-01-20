284aa6cd44439a02a081ece089c54ace90694b8f
qpvale34 <qpvale34@users.noreply.github.com>
2026-01-20 23:22:45 +0300

    Update basePath for new repo name

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  basePath: "/mysite",
+  basePath: "/ibrahimertugrul",
   images: {
     unoptimized: true,
   },
